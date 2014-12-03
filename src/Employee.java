170be2e2b12df85e70f74397fed5d5c50701b637
mama-egg <Kinseygoesrawr@gmail.com>
2014-12-03 08:19:53 -0700

    Update Employee.java

--- src/Employee.java
+++ src/Employee.java
@@ -1,1 +1,46 @@
 // placeholder for your Employee class
+public class Employee {
+  
+  public Employee(String name, double wage, int hours) {
+    
+    public String getName() {
+      return name;
+    }
+    
+    public double getWage() {
+      return wage;
+    }
+    
+    public int getHours() {
+      return hours;
+    }
+    
+    public double getWeeklySalary() {
+      return (hours*wage);
+    }
+    
+    public double getYearlySalary() {
+      return (hours * wage * 52);
+    }
+    
+    public String toString() {
+      return ("name: " + name + ", wage: " + wage + ", hours: " + hours);
+    }
+    
+    public setName(String newname) {
+      name = newname;
+    }
+    
+    public void setWage(double newWage) {
+      wage = newWage;
+    }
+    
+    public void setHours(int h) {
+      hours = h;
+    }
+    
+    public String giveRaise(String newname) {
+      return ()
+    }
+  }
+}
