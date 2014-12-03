a8b79b85a7d1c32be29646646bbd6e72d1882c23
mama-egg <Kinseygoesrawr@gmail.com>
2014-12-03 08:19:46 -0700

    Update EmployeeWages.java

--- src/EmployeeWages.java
+++ src/EmployeeWages.java
@@ -1,28 +1,24 @@
 // placeholder for your main method
 public class EmployeeWages {
     public static void main(String[] args) {
-        // replace <put your name here> with your name!
-        System.out.println("Student: <put your name here>");
         
-        // Create two instances of class Employee emp1 & emp2
+        System.out.println("Student: Kinsey");
         
-        // print emp1 
+        Employee emp1 = new Employee(){
+            System.out.print(emp1.toString);
+            System.out.print(\t "weekly salary: $" + emp1.getWeeklySalary());
+            System.out.print(\t "annual salary: $" + emp1.getAnnualSalary());
+        }
         
-        // print emp1's weekly salary (put a tab first) like "       weekly salary: $360.00" 
-        
-        // print emp1's annual salary (put a tab first) like "       annual salary: $18,720.00" 
-        
-        // print emp2
-        
-        // change emp2's name
-        
-        // change emp2's wage
-        
-        // change emp2's hours
-        
-        // print emp2 
-        
-        // print emp2's weekly salary (put a tab first) like "       weekly salary: $360.00" 
+        Employee emp2 = new Employee(); {
+            System.out.println(emp2.toString());
+            emp2.setName(newname);
+            emp2.setWage(newWage);
+            emp2.setHours(int h);
+            System.out.println(emp2.toString());
+            System.out.print(\t "weekly salary: $" + emp2. )
+        }
+// print emp2's weekly salary (put a tab first) like "       weekly salary: $360.00" 
         
         // print emp2's annual salary (put a tab first) like "       annual salary: $18,720.00" 
         
